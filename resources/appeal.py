c16f8ad71940e07dbf37ac4359f9ee68313ff564
ingecraft <ingenious.craftsman@gmail.com>
2020-04-13 17:01:44 +0300

    Added resource structure to lead

---
+++ resources/appeal.py
@@ -0,0 +1,16 @@
+from flask_resftul import Resource
+from models.appeal import AppealModel
+
+
+class Appeal(Resource):
+    def get(self, _id):
+        pass
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass

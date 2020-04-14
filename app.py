762aa78f64c45f41d42b3ffa45c75dde68a27285
ingecraft <ingenious.craftsman@gmail.com>
2020-04-14 17:40:45 +0300

    Implemented app with basic config and environmental variables

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
+from config import config
 from db import db
 from resources.operator import Operator, OperatorList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://spike:tatoum1983@localhost/pepito-dev'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'bandit'
+app.config.from_object(config['development'])
 
 
 @app.before_first_request
